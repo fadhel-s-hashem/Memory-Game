0ef6eb5a0b01b6219bf044eae3a9f44edfa8671f
Fadhel <fadhel.s.hashem@gmail.com>
2026-06-27 15:35:38 +0300

    shuffle emohi in squares

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const sqr = document.querySelectorAll('.sqr')
 const result = document.querySelector('#result')
 const board = document.querySelector('.board')
-const reset = document.querySelector('#reset')
+const resetBT = document.querySelector('#reset')
 const startBT = document.querySelector('#start')
+
+const emojis = ['🍎' ,'🍎','🍌','🍌','🍇', '🍇','🍒','🍒','🍉',
+    '🍉','🍍','🍍'];
+
+let randomImoji = emojis.sort(function() {
+return 0.5-Math.random() 
+}
+)
+
+sqr.forEach(function(card,arry){
+    card.addEventListener('click', function(){
+        if(card.textContent !=="") {return}}
+    )
+    card.textContent=randomImoji[arry];
+})
