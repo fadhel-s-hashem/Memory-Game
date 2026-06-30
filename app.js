395da8c56b4cfa8af5d10f798161a41eeee0bbef
Fadhel <fadhel.s.hashem@gmail.com>
2026-06-30 12:47:31 +0300

    check pioint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const time = document.querySelector('#timer')
 let userChoice = [];
 let gameStarte = false;
 
+let pairsFound = 0
 let timeRemain;
 let timerOn= 30;
 
@@ -19,6 +20,7 @@ return 0.5-Math.random()
 })
 
 sqr.forEach(function(card,arry){     
+
     card.textContent=randomImoji[arry]
 
      card.addEventListener('click', function() {
@@ -28,7 +30,6 @@ sqr.forEach(function(card,arry){
         card.textContent = randomImoji[arry]
     }
 
-      
       userChoice.push(card)
       if (userChoice.length === 2) {
         let firstCard = userChoice[0]
@@ -37,6 +38,12 @@ sqr.forEach(function(card,arry){
       if(firstCard.textContent===secondCard.textContent){
         firstCard.style.backgroundColor = 'lightgreen'
         secondCard.style.backgroundColor = 'lightgreen'
+pairsFound = pairsFound +1
+if (pairsFound === 6) {
+        result.textContent = "You win"
+        clearInterval(calculate)
+       }
+console.log(pairsFound);
         userChoice = []
       } else{ if(firstCard.textContent !== secondCard.textContent){
         setTimeout(function(){
@@ -47,8 +54,7 @@ sqr.forEach(function(card,arry){
       }}
     }
         
-    
-            
+             
  })    
 })
 
@@ -59,16 +65,28 @@ startBT.addEventListener('click',function(){
     
     })
 
-    const runTimer = setInterval(function(){
-       time.textContent = timerOn = timerOn-1
-    },1000)
+//     const runTimer = setInterval(function(){
+//        time.textContent = timerOn = timerOn-1
+//     },1000)
 
-    setTimeout(function(){
-      if(timerOn <= 0 ){
-      result.textContent="Game Over"
-      clearInterval(runTimer)
+// setTimeout(function(){
+//        if(timerOn <= 0 && pairsFound !== 6){
+//       result.textContent="Game Over"
+//       clearInterval(runTimer)
+//        } 
+// },30000)
 
-      }},30000)
+   const calculate = setInterval(displayTime, 1000)
+
+    function displayTime() {
+      time.textContent = timerOn = timerOn-1
+
+      if(timerOn <=0 ){
+      clearInterval(calculate)
+      result.textContent="Game over"
+      }
+    }
+      
 
       
       
